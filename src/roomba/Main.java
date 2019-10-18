52a16a8fe40dd0dcdd23b1e11ddab2f7eeea1520
Francisco García <frangaren@usal.es>
2019-10-18 19:42:26 +0200

    Añadido prueba de movimiento

--- src/roomba/Main.java
+++ src/roomba/Main.java
@@ -5,14 +5,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
         RoombaComm roomba = new RoombaCommSerial();
-        boolean 
-        while()
-        System.out.print("Puerto: ");
-        String port = sc.next();
-        roomba.connect(port);
+        boolean connected = false;
+        String[] ports = roomba.listPorts();
+        do {
+            System.out.println("Puertos:");
+            for (int i = 0; i < ports.length; i++) {
+                System.out.println((i + 1) + " - " + ports[i]);
+            }
+            System.out.print("> ");
+            int port = sc.nextInt();
+            connected = roomba.connect(ports[port - 1]);
+            if (!connected) {
+                System.out.println("Puerto inválido.");
+            }
+        } while(!connected);
+        roomba.startup();
+        roomba.goForward();
+        Thread.sleep(1000);
+        roomba.stop();
+        roomba.disconnect();
     }
 
 }

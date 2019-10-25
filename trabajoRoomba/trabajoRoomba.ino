4eed3f08e1c65b6bbe683925ba8ad50698645067
Alvaro Torijano <alvarotorijano@usal.es>
2019-10-25 18:28:12 +0200

    Aun no hace el face del objetivo bien

--- trabajoRoomba/trabajoRoomba.ino
+++ trabajoRoomba/trabajoRoomba.ino
@@ -1,46 +1,47 @@
 /*
   Software serial multple serial test
 
- Receives from the two software serial ports,
- sends to the hardware serial port.
+  Receives from the two software serial ports,
+  sends to the hardware serial port.
 
- In order to listen on a software port, you call port.listen().
- When using two software serial ports, you have to switch ports
- by listen()ing on each one in turn. Pick a logical time to switch
- ports, like the end of an expected transmission, or when the
- buffer is empty. This example switches ports when there is nothing
- more to read from a port
+  In order to listen on a software port, you call port.listen().
+  When using two software serial ports, you have to switch ports
+  by listen()ing on each one in turn. Pick a logical time to switch
+  ports, like the end of an expected transmission, or when the
+  buffer is empty. This example switches ports when there is nothing
+  more to read from a port
 
- The circuit:
- Two devices which communicate serially are needed.
- * First serial device's TX attached to digital pin 10(RX), RX to pin 11(TX)
- * Second serial device's TX attached to digital pin 8(RX), RX to pin 9(TX)
+  The circuit:
+  Two devices which communicate serially are needed.
+   First serial device's TX attached to digital pin 10(RX), RX to pin 11(TX)
+   Second serial device's TX attached to digital pin 8(RX), RX to pin 9(TX)
 
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+  Note:
+  Not all pins on the Mega and Mega 2560 support change interrupts,
+  so only the following can be used for RX:
+  10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
 
- Not all pins on the Leonardo support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+  Not all pins on the Leonardo support change interrupts,
+  so only the following can be used for RX:
+  8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
 
- created 18 Apr. 2011
- modified 19 March 2016
- by Tom Igoe
- based on Mikal Hart's twoPortRXExample
+  created 18 Apr. 2011
+  modified 19 March 2016
+  by Tom Igoe
+  based on Mikal Hart's twoPortRXExample
 
- This example code is in the public domain.
+  This example code is in the public domain.
 
- */
+*/
 
 // software serial #1: RX = digital pin 10, TX = digital pin 11
 
 
-
+#define _SS_MAX_RX_BUFF 90 // RX buffer size
+#include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
-#include <LCD.h>
+#include <LCD.h><z
 #include <LiquidCrystal_I2C.h>  // F Malpartida's NewLiquidCrystal library
 
 /*-----( Declare Constants )-----*/
@@ -51,6 +52,17 @@
 
 #define  LED_OFF  0
 #define  LED_ON  1
+
+#define TX_ROOMBA_PIN 4
+#define RX_ROOMBA_PIN 3
+#define DETECTION_THRESHOLD 15000
+
+#define SAFE true
+#define FULL false
+
+#define FULL_TURN_TIME 3303
+
+#define SENSOR_REFRESH_RATE 250
 
 //mjkdz i2c LCD board
 //                   addr, en,rw,rs,d4,d5,d6,d7,bl, blpol
@@ -70,8 +82,8 @@ typedef struct sensors {
 	byte unused1;
 	byte irOpCode;
 	byte buttons;
-	int16_t distance;	//Acumulative 
-	int16_t angle;		//Acumulative 
+	int16_t distance;	//Acumulative
+	int16_t angle;		//Acumulative
 	byte chargingState;
 	int16_t voltage;
 	int16_t current;
@@ -94,8 +106,8 @@ typedef struct sensors {
 	int16_t radius;
 	int16_t velocityRight;
 	int16_t velocityLeft;
-	uint16_t encoderCountsLeft;  //Acumulative 
-	uint16_t encoderCountsRight; //Acumulative 
+	uint16_t encoderCountsLeft;  //Acumulative
+	uint16_t encoderCountsRight; //Acumulative
 	byte lightBumper;
 	uint16_t lightBumpLeft;
 	uint16_t lightBumpfrontLeft;
@@ -110,21 +122,11 @@ typedef struct sensors {
 	int16_t mainBrushCurrent;
 	int16_t sideBrushCurrent;
 	byte stasis;
-}sensorPack_t;
-
-#define TX_ROOMBA_PIN 4
-#define RX_ROOMBA_PIN 3
-
-#define SAFE true
-#define FULL false
-
-#define FULL_TURN_TIME 3303
-
-#define SENSOR_REFRESH_RATE 250
+} sensorPack_t;
 
 SoftwareSerial myseruial(TX_ROOMBA_PIN, RX_ROOMBA_PIN);
 
-void updateState(sensorPack_t reading, sensorPack_t * state) {
+void updateState(sensorPack_t reading, sensorPack_t* state) {
 	reading.encoderCountsLeft += state->encoderCountsLeft;
 	reading.encoderCountsRight += state->encoderCountsRight;
 	reading.distance += state->distance;
@@ -133,32 +135,33 @@ void updateState(sensorPack_t reading, sensorPack_t * state) {
 }
 
 void roombaInit(bool safe) {
-	byte start[2] = { 128, 132 };
-	if (safe) {
-		start[1] = 130;
-	}
-	
-	myseruial.write(start, 2);
-	readAngle();
+	byte start[3] = { 128, 130, 132 };
+	/*
+	  if (safe) {
+	  start[1] = 130;
+	  }
+	*/
+	myseruial.write(start, 3);
 }
 
 void stopMoving() {
 	byte stop[5] = { 137, 0, 0, 0, 0 };
+	myseruial.write(stop, 5);
 }
 
 // Comienza el movimiento velocidad tiene que estar en -500 a 500 mm/s y el angulo entre -2000 y 2000 mm
-void drive(word velocity, word angle) {
+void drive(uint16_t velocity, uint16_t angle) {
 	byte Hvelocity = velocity & 0xFF00;
 	byte Lvelocity = velocity & 0x00FF;
 
 	byte Hangle = angle & 0xFF00;
 	byte Langle = angle & 0x00FF;
 
-	byte data[5] = { 
-		137, // Comando de mover
-		Hvelocity, Lvelocity, // 2 bytes de velocidad
-		Hangle, Langle // 2 bytes de angulo
-		};
+	byte data[5] = {
+	  137, // Comando de mover
+	  Hvelocity, Lvelocity, // 2 bytes de velocidad
+	  Hangle, Langle // 2 bytes de angulo
+	};
 	myseruial.write(data, 5);
 }
 
@@ -180,7 +183,7 @@ int16_t readAngle() {
 	byte MSB, LSB;
 	myseruial.write(angleRead, 2);
 	while (myseruial.available() < 2) {
-		delay(20);
+		delay(200);
 	}
 
 	MSB = myseruial.read();
@@ -191,58 +194,181 @@ int16_t readAngle() {
 
 sensorPack_t readSensors() {
 	sensorPack_t reading;
-	byte readAllSensors[2] = { 142, 100 };
-	byte buffer[80]; //Would be possible to use buffer[sizeof(...)] after the C99 standar
+	byte readSensorPack6[2] = { 142, 6 };
+	byte readSensorPack101[2] = { 142, 101 };
+	byte buffert[120]; //Would be possible to use buffer[sizeof(...)] after the C99 standar
 	byte i = 0;
-	while (myseruial.available() < sizeof(sensorPack_t)) {
-		delay(20);
-	}
-	for (i = 0; i < sizeof(sensorPack_t); i++) {
-		buffer[i] = myseruial.read();
-	}
-	memcpy(&reading, buffer, sizeof(sensorPack_t));
 
+	myseruial.write(readSensorPack6, 2);
+	delay(20);
+
+	while (myseruial.available()) {
+		buffert[i++] = myseruial.read();
+	}
+
+	myseruial.write(readSensorPack101, 2);
+	delay(20);
+
+	while (myseruial.available()) {
+		buffert[i++] = myseruial.read();
+	}
+
+
+	memcpy(&reading, buffert, i);
+	Serial.print("Estos son mis bytes: ");
+	Serial.print(i);
 	return reading;
 }
+
 void readSensors(sensorPack_t* data) {
 	(*data) = readSensors();
 }
 
+/*
+  void showSensors(sensorPack_t data) {
+	char salida[1300];
+	memset(salida, 0, 1300);
+	sprintf(salida, "\n sWheelDrops: %d\n : %d\n fLeft: %d\n fFroeft: %d\n fFht: %d\n fRight: %d\n ualWall: %d\n cus: %d\n Dt: %d\n ed1: %d\n Code: %d\n ons: %d\n ance : %d\n e : %d\n gingState: %d\n age: %d\n ent: %d\n erature: %d\n eryCharge: %d\n eryCapacity: %d\n Signal: %d\n fLeftSignal: %d\n fFrnal: %d\n fFrognal: %d\n fRal: %d\n ed2: %d\n ed3: %d\n geable: %d\n Ide: %d\n Number: %d\n Playing: %d\n rekets: %d\n city: %d\n us: %d\n cityRight: %d\n cityLeft: %d\n derCosLeft : %d\n derRight : %d\n tBper: %d\n tBpLeft: %d\n tBptLeft: %d\n tBpCenft: %d\n tBpCeht: %d\n tBumpfRight: %d\n tBumpRight: %d\n CodeLeft: %d\n CodeRight: %d\n Morent: %d\n tMott: %d\n Brent: %d\n Brt: %d\n is: %d", data.bumpsWheelDrops, data.wall, data.cliffLeft, data.cliffFrontLeft, data.cliffFrontRight, data.cliffRight, data.virtualWall, data.overcurrents, data.dirtDetect, data.unused1, data.irOpCode, data.buttons, data.distance, data.angle, data.chargingState, data.voltage, data.current, data.temperature, data.batteryCharge, data.batteryCapacity, data.wallSignal, data.cliffLeftSignal, data.cliffFrontLeftSignal, data.cliffFrontRightSignal, data.cliffRightSignal, data.unused2, data.unused3, data.chargerAvailable, data.openInterfaceMode, data.songNumber, data.songPlaying, data.OIStreamNumPackets, data.velocity, data.radius, data.velocityRight, data.velocityLeft, data.encoderCountsLeft, data.encoderCountsRight, data.lightBumper, data.lightBumpLeft, data.lightBumpfrontLeft, data.lightBumpCenterLeft, data.lightBumpCenterRight, data.lightBumpfrontRight, data.lightBumpRight, data.irOPCodeLeft, data.irOPCodeRight, data.leftMotorCurrent, data.rightMotorCurrent, data.mainBrushCurrent, data.sideBrushCurrent, data.stasis);
+	Serial.print(salida);
+  }
+*/
+
 void setup() {
-  // Open serial communications and wait for port to open:
+	static sensorPack_t state;
+	// Open serial communications and wait for port to open:
 	Serial.begin(USB_BAUDRATE);
+	myseruial.begin(115200);
+
+	Serial.println("Leonidas bot");
 	lcd.begin(16, 2); // inicializar lcd
 	lcd.clear();
 	lcd.setCursor(0, 0);
 	lcd.print("  Leonidas Bot");
-	lcd.setCursor(0, 1);
-	lcd.print(sizeof(sensorPack_t));
 
-	myseruial.begin(115200);
-	roombaInit(FULL);	
-	
-	lcd.setCursor(0, 1);
-	lcd.print(readAngle());
-  // Start each software serial port
+	roombaInit(FULL);
+	delay(100);
+	//turnDegree(360, true);
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("Leyendo sensores");
+	delay(2000);
+	state = readSensors(); //reset senswor readings
+	//showSensors(state);
 
-	
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("Buscando: ");
+	delay(2000);
+	//byte clockwise[5] = { 137, 0, 25, 255, 255 };
+	//myseruial.write(clockwise, 5);
+	//delay(5000);
+	//drive(25, 0xffff);
+	//delay(5000);
 }
 
-void loop() {
+void showIr(sensorPack_t state) {
+	myseruial.println("lightBumpLeft: ");
+	myseruial.print(state.lightBumpLeft);
+	myseruial.println("lightBumpfrontLeft: ");
+	myseruial.print(state.lightBumpfrontLeft);
+	myseruial.println("lightBumpCenterLeft: ");
+	myseruial.print(state.lightBumpCenterLeft);
+	myseruial.println("lightBumpCenterRight: ");
+	myseruial.print(state.lightBumpCenterRight);
+	myseruial.println("lightBumpfrontRight: ");
+	myseruial.print(state.lightBumpfrontRight);
+	myseruial.println("lightBumpRight: ");
+	myseruial.print(state.lightBumpRight);
+}
 
-	static long double last_sensor_reading = millis();
+enum strategy {
+	START = 0, SEEK, FACE, PUSH, CENTER
+};
+
+void loop() {
+	static bool updated = false;
+	byte clockwise[5] = { 137, 0, 25, 255, 255 };
+	static enum strategy phase = START;
+	static long double last_sensor_reading = millis(), last_lcd_update = millis();
 	static sensorPack_t state;
+	int i;
 
 	if ((millis() - last_sensor_reading) > SENSOR_REFRESH_RATE) {
 		updateState(readSensors(), &state);
+		last_sensor_reading = millis();
 	}
+
+	if ((millis() - last_lcd_update) > SENSOR_REFRESH_RATE) {
+
+		lcd.setCursor(0, 1);
+		lcd.print("                ");
+		lcd.setCursor(0, 1);
+		lcd.print(state.lightBumpRight);
+
+		last_lcd_update = millis();
+	}
+
 	if (state.cliffFrontLeft || state.cliffFrontRight || state.cliffLeft || state.cliffRight) {
 		stopMoving();
+		lcd.clear();
+		lcd.print("Que me caigo!");
+		while (1);
 	}
 
-  // blank line to separate data from the two ports:
-  // Serial.println();
+	switch (phase) {
+	case START:
+		if (!updated) {
+			lcd.clear();
+			lcd.print("Start");
+			updated = true;
+		}
 
+		myseruial.write(clockwise, 5);
+		delay(2000);
+		phase = SEEK;
+		updated = false;
+		break;
+
+	case SEEK:
+		if (!updated) {
+			lcd.setCursor(0, 0);
+			lcd.print("SEEK         ");
+			updated = !updated;
+		}
+		if (state.lightBumpRight > DETECTION_THRESHOLD) {
+			stopMoving();
+			lcd.setCursor(0, 0);
+			lcd.print("Found");
+			delay(5000);
+			phase = FACE;
+			updated = false;
+		}
+		break;
+
+	case FACE:
+		if (!updated) {
+			lcd.clear();
+			lcd.print("Face");
+			updated = !updated;
+		}
+		turnDegree(60, false);
+		phase = PUSH;
+		updated = false;
+		break;
+
+	case PUSH:
+		if (!updated) {
+			lcd.clear();
+			lcd.print("Pushing");
+			updated = !updated;
+		}
+		drive(25, 0);
+		delay(2000);
+		break;
+
+	}
+
+	
 	delay(1);
-}
 
+}
